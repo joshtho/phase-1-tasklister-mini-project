d898de51b11b8c4d31faef869a440f8fa9d269b3
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-03-18 23:12:27 -0500

    assignment complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+ document.querySelector('form').addEventListener("submit", (e) => {
+   e.preventDefault()
+   handleToDo(e.target.grabable.value) 
+   const targs = 
+   console.log(e.target)
+ }) // your code here
+})
+ 
+function handleToDo(todo) {
+  let p = document.createElement('p')
+p.textContent = todo
+document.querySelector('#tasks').appendChild(p);
+}
